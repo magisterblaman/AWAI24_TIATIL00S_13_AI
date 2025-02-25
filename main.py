9e90116a97cb9530b5f95c42a4d295a4dbd428e6
magisterblaman <hannes.blaman@lbs.se>
2025-02-25 10:02:35 +0100

    AI med minne

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ import json
 
 import requests
 
-question = input("What do you wonder? ")
+messages = []
 
-my_headers = {
-    "Authorization": "Bearer <key>" #klista in din OpenRouter-nyckel
-}
+while True:
+    question = input("What do you wonder? ")
 
-my_body = {
-    "model": "google/gemini-flash-1.5-8b-exp",
-    "messages": [
-        {
-            "role": "user",
-            "content": question
-        }
-    ]
-}
+    messages.append({
+        "role": "user",
+        "content": question
+    })
 
-result = requests.post("https://openrouter.ai/api/v1/chat/completions",
-                       headers=my_headers,
-                       data=json.dumps(my_body))
-result_json = result.json()
+    my_headers = {
+        "Authorization": "Bearer <key>" #klista in din OpenRouter-nyckel
+    }
 
-if "error" in result_json:
-    print("Error!")
-else:
-    content = result_json["choices"][0]["message"]["content"]
+    my_body = {
+        "model": "mistralai/mistral-7b-instruct:free",
+        "messages": messages
+    }
 
-    print(content)
+    result = requests.post("https://openrouter.ai/api/v1/chat/completions",
+                           headers=my_headers,
+                           data=json.dumps(my_body))
+    result_json = result.json()
+
+    if "error" in result_json:
+        print("Error!")
+    else:
+        answer = result_json["choices"][0]["message"]
+        messages.append(answer)
+        content = answer["content"]
+
+        print(content)
